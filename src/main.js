a231dcc41623fc9698613ecf7fa966ff050efdd7
NethanosGit <razudesu31@gmail.com>
2023-10-30 19:02:06 +0200

    Router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import "./registerServiceWorker";
+import router from './router'
 loadFonts();
 
-createApp(App).use(vuetify).mount("#app");
+createApp(App).use(router).use(vuetify).mount("#app");
