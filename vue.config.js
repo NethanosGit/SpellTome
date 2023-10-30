f7440c869561394ea801855ffec8970dbc72cb63
NethanosGit <razudesu31@gmail.com>
2023-10-30 17:42:44 +0200

    Vuetify

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  pwa: {
-    workboxPluginMode: "GenerateSW",
-  },
-  base: "/SpellTome/",
-  transpileDependencies: true,
-});
