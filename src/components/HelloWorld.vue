c7973969e615ad7060b14ee85a2bae70355263a2
NethanosGit <razudesu31@gmail.com>
2023-10-30 17:44:08 +0200

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
+          Welcome to the Vuetify 3 Beta Website
         </h1>
 
         <h4>Vite Preview</h4>
