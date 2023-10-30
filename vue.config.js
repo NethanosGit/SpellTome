5a292416d052abe4f731eb5e464c7e2bc6bad289
NethanosGit <razudesu31@gmail.com>
2023-10-30 18:01:26 +0200

    Vuetify

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,13 @@ module.exports = defineConfig({
   pwa: {
     workboxPluginMode: "GenerateSW",
   },
+
   publicPath: process.env.NODE_ENV === "production" ? "/SpellTome/" : "/",
   transpileDependencies: true,
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
 });
