663f695f55f03faadf08a98690cec3d105f2731c
NethanosGit <razudesu31@gmail.com>
2023-10-31 17:41:48 +0200

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,28 @@
 <template>
   <v-app-bar
     class="text-white"
+    :border="true"
     color="transparent"
     density="compact"
-    :elevation="1"
+    :elevation="0"
   >
-    <v-app-bar-nav-icon class="ml-6">
-      <img src="../img/logos/DND-logo.png" class="dnd-logo" />
-    </v-app-bar-nav-icon>
-
-    <v-btn-toggle
-      v-model="navSelection"
-      rounded="1"
-      density="compact"
-      variant="text"
-      color="red"
-      class="text-brownish"
-      group
-    >
-      <v-btn value="spells" to="/">Spells</v-btn>
-      <v-btn value="rules" to="/rules">Rules</v-btn>
-      <v-btn value="settings" to="/settings">Settings</v-btn>
-    </v-btn-toggle>
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon>
+        <img src="../img/icons/android-chrome-192x192.png" class="dnd-logo" />
+      </v-app-bar-nav-icon>
+      <v-app-bar-title text="SpellCraft"></v-app-bar-title>
+      <v-btn-toggle
+        v-model="navSelection"
+        rounded="1"
+        density="compact"
+        variant="text"
+        color="red"
+        class="text-brownish mx-3"
+        group
+      >
+        <v-btn value="spells" to="/">Spells</v-btn>
+      </v-btn-toggle>
+    </template>
   </v-app-bar>
 </template>
 
@@ -32,6 +33,6 @@ const navSelection = ref("spells");
 </script>
 <style>
 .dnd-logo {
-  width: 60px;
+  width: 42px;
 }
 </style>
